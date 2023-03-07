282b03485b7b21308a5fbf55d5d4ce90e0f5ca8d
alexangria1992 <alex040892@gmail.com>
2023-03-07 13:38:32 +1300

    Custom React Hook (Currency)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <App />
-        <ReactQueryDevtools></ReactQueryDevtools>
+        <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
