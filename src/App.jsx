af11d9d69f29e1cb5fa12976aaf0dbdbad46e366
alexangria1992 <alex040892@gmail.com>
2023-03-07 15:27:53 +1300

    App Gradient Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Flex } from "@chakra-ui/react";
 import Converter from "../features/Converter";
 
 function App() {
   return (
-    <div>
-      <Converter />
-    </div>
+    <>
+      <Flex
+        bgGradient="linear(to-t, #ae085c, #2e0656)"
+        justifyContent="center"
+        height="100vh"
+      >
+        <Converter />
+      </Flex>
+    </>
   );
 }
 
